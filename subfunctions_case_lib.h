f97f4b87d4fbca2990c16bd9ee59b679a866b8a5
arina udina <arinaudina@MacBook-Air-arina.local>
2025-11-14 22:10:48 +0300

    initialized functions from auxiliary files in the case

---
+++ subfunctions_case_lib.h
@@ -0,0 +1,11 @@
+#ifndef SUBFUNCTIONS_CASE_LIB_H
+#define SUBFUNCTIONS_CASE_LIB_H
+
+double sum_cos(double *x, int *step, double *array);
+double sum_sin(double *x, int *step, double *array);
+
+double input(int *step);
+double print(double sum, double *array, int step);
+
+
+#endif
